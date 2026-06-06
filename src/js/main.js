9feaa8e6ed6c4da74abbe0869b1a53732a32094e
Kirill <grinders92@gmail.com>
2026-06-07 00:24:06 +0300

    add homework files

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './render-functions.js';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const query = form.elements['search-text'].value.trim();
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(({ data }) => {
+      hideLoader();
+
+      if (data.hits.length === 0) {
+        iziToast.show({
+          icon: 'ico-error',
+          iconColor: '#fff',
+          title: 'Error',
+          titleColor: '#fff',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#fff',
+          backgroundColor: '#ef4040',
+          position: 'topRight',
+          timeout: 5000,
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(err => {
+      hideLoader();
+      iziToast.show({
+        icon: 'ico-error',
+        iconColor: '#fff',
+        title: 'Error',
+        titleColor: '#fff',
+        message: err.message,
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+        timeout: 5000,
+      });
+    });
+});
